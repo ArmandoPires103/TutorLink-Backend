7468c5f3f0d1e89d2c963d7429d9f1147966b462
Haiyah Perez <haiyahperez@pursuit.org>
2024-03-25 15:46:39 -0400

    tutor requests

--- queries/requests.js
+++ queries/requests.js
@@ -5,7 +5,7 @@ const getRequestsById = async (tutorId) => {
     console.log("query", tutorId)
     try {
         const tutorRequests = await db.any(
-            "SELECT student.username AS student_name, student.id AS student_id, requests.accepted FROM requests INNER JOIN users AS tutor ON requests.tutor_id = tutor.id INNER JOIN users AS student ON requests.user_id = student.id WHERE tutor.id = $1",
+            "SELECT student.username AS student_name, student.id AS student_id, student.email AS student_email, student.profile_pic AS student_profile_pic, requests.accepted FROM requests INNER JOIN users AS tutor ON requests.tutor_id = tutor.id INNER JOIN users AS student ON requests.user_id = student.id WHERE tutor.id = $1",
             [tutorId]
         );
         console.log(tutorRequests)
