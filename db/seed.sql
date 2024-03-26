567ecc80accd1667c533cbee6abfd25b42027e3d
armandopires <armandopires@pursuit.org>
2024-03-26 13:02:11 -0400

    Added photos

--- db/seed.sql
+++ db/seed.sql
@@ -15,20 +15,20 @@ VALUES
 -- password 1: password
 -- password 2: password2
 -- password 3: password3
-    (TRUE, 'profile1.jpg', 'demo_tutor', '$2b$10$.z68x3792U9LyBwmghfsKexstMO7i0SeNCoDmeJa7bEFPQBnZU3bK', 'demo_tutor@example.com', 'Jane Doe', FALSE, TRUE, 'Mathematics','Experienced tutor specializing in mathematics. Patient and dedicated to helping students achieve their goals.', TRUE),
+    (TRUE, 'https://res.cloudinary.com/dhexjuuzd/image/upload/v1711469497/Data%20seed%20class/tutor1_cxrea6.webp', 'demo_tutor', '$2b$10$.z68x3792U9LyBwmghfsKexstMO7i0SeNCoDmeJa7bEFPQBnZU3bK', 'demo_tutor@example.com', 'Jane Doe', FALSE, TRUE, 'Mathematics','Experienced tutor specializing in mathematics. Patient and dedicated to helping students achieve their goals.', TRUE),
 
-    (TRUE, 'tutor2.jpg', 'tutor2', '$2b$10$FnW6j217LqxhnlkAYf1SAuiFeCNUGmBaQe9gLVPK9dhQk1Af/L9vy', 'tutor2@example.com', 'Jane Smith', FALSE, TRUE, 'Physics', 'Experienced tutor specializing in physics. Patient and committed to guiding students towards mastery of complex concepts and problem-solving skills.',TRUE),
+    (TRUE, 'https://res.cloudinary.com/dhexjuuzd/image/upload/v1711469507/Data%20seed%20class/tutor_2_fruod4.jpg', 'tutor2', '$2b$10$FnW6j217LqxhnlkAYf1SAuiFeCNUGmBaQe9gLVPK9dhQk1Af/L9vy', 'tutor2@example.com', 'Jane Smith', FALSE, TRUE, 'Physics', 'Experienced tutor specializing in physics. Patient and committed to guiding students towards mastery of complex concepts and problem-solving skills.',TRUE),
 
-    (TRUE, 'tutor3.jpg', 'tutor3', '$2b$10$KHZo4CN1vFYDs3E.W5SLzOVkKzbeu91mIHOCUYoS/Z2Q1zAClaSeG', 'tutor3@example.com', 'Alice Johnson', FALSE, TRUE, 'Chemistry', 'Experienced tutor specializing in chemistry. Patient and dedicated to assisting students in understanding the intricacies of chemical principles and fostering their problem-solving abilities.',TRUE),
+    (TRUE, 'https://res.cloudinary.com/dhexjuuzd/image/upload/v1711469511/Data%20seed%20class/tutor_3_nditky.jpg', 'tutor3', '$2b$10$KHZo4CN1vFYDs3E.W5SLzOVkKzbeu91mIHOCUYoS/Z2Q1zAClaSeG', 'tutor3@example.com', 'Alice Johnson', FALSE, TRUE, 'Chemistry', 'Experienced tutor specializing in chemistry. Patient and dedicated to assisting students in understanding the intricacies of chemical principles and fostering their problem-solving abilities.',TRUE),
 -- Students - users.id 4,5,6
 --password 4: password
 --password 5: password5
 --password 6: password6
-    (FALSE, 'profile2.jpg', 'demo_student', '$2b$10$L5QiYCI6r9hddL3aRdAWTO6c7wj6aXgsjSCaDJ00kLWnwgTJ9o4TG', 'demo_student@example.com', 'John Smith', TRUE, FALSE, 'Mathematics', NULL, TRUE),
+    (FALSE, 'https://res.cloudinary.com/dhexjuuzd/image/upload/v1711472464/Data%20seed%20class/student_1_qjnrah.jpg', 'demo_student', '$2b$10$L5QiYCI6r9hddL3aRdAWTO6c7wj6aXgsjSCaDJ00kLWnwgTJ9o4TG', 'demo_student@example.com', 'John Smith', TRUE, FALSE, 'Mathematics', NULL, TRUE),
 
-    (FALSE, 'student2.jpg', 'student2', '$2b$10$tHaKfLOvdy472Y0RvEQPue7RcGktFP8oUDKntM3jGOOMSpz4eChwe', 'student1@example.com', 'Michael Johnson', TRUE, FALSE, 'Physics', NULL, TRUE),
+    (FALSE, 'https://res.cloudinary.com/dhexjuuzd/image/upload/v1711472470/Data%20seed%20class/student_2_ooq6vn.jpg', 'student2', '$2b$10$tHaKfLOvdy472Y0RvEQPue7RcGktFP8oUDKntM3jGOOMSpz4eChwe', 'student1@example.com', 'Michael Johnson', TRUE, FALSE, 'Physics', NULL, TRUE),
 
-    (FALSE, 'student3.jpg', 'student3', '$2b$10$nZ4/GQomk6CNPXz00tODu.0YycaOxNcEVaAeCiHoeosjbXk/S0iua', 'student2@example.com', 'Emma Wilson', TRUE, FALSE, 'Chemistry', NULL, TRUE)
+    (FALSE, 'https://res.cloudinary.com/dhexjuuzd/image/upload/v1711472477/Data%20seed%20class/student_3_wyafvc.jpg', 'student3', '$2b$10$nZ4/GQomk6CNPXz00tODu.0YycaOxNcEVaAeCiHoeosjbXk/S0iua', 'student2@example.com', 'Emma Wilson', TRUE, FALSE, 'Chemistry', NULL, TRUE)
 ;
 
 -- USER ID IS STUDENT ID
