c0be1a49fa6f5f103acdb62c5d91b4cd0da07f6b
juligarc91 <julissagarcia@pursuit.org>
2024-03-26 15:28:12 -0400

    created edit and delete routes and queries for reviews

--- queries/studentreviews.js
+++ queries/studentreviews.js
@@ -21,33 +21,65 @@ VALUES (1, 'Mathematics', 'Great tutor, very helpful with calculus problems.', 4
 ;
 */
 
-const createStudentReview = async (
-  assigned_tutor_id,
-  subject,
-  description,
-  user_id,
-  ratings
-) => {
+const createStudentReview = async (review) => {
   try {
-    const { assigned_tutor_id, subject, description, user_id, ratings } =
-      reviewData;
     const query = `
-            "INSERT INTO student_reviews (assigned_tutor_id, subject, description, user_id, ratings) VALUES ($1, $2, $3, $4, $5) RETURNING *`;
+      INSERT INTO student_reviews (tutor_id, subject, description, user_id, ratings)
+      VALUES ($1, $2, $3, $4, $5) RETURNING *`;
     const newReview = await db.one(query, [
-      assigned_tutor_id,
-      subject,
-      description,
-      user_id,
-      ratings,
+      review.tutor_id,
+      review.subject,
+      review.description,
+      review.user_id,
+      review.ratings,
     ]);
     return newReview;
   } catch (error) {
-    console.error("Error creating user:", error);
+    console.error("Error creating review:", error);
     throw error;
   }
 };
 
+const updateStudentReview = async (reviewId, updatedReview) => {
+  try {
+    const query = `
+      UPDATE student_reviews
+      SET tutor_id = $1, subject = $2, description = $3, user_id = $4, ratings = $5
+      WHERE id = $6
+      RETURNING *`;
+    const updatedReviewData = await db.one(query, [
+      updatedReview.tutor_id,
+      updatedReview.subject,
+      updatedReview.description,
+      updatedReview.user_id,
+      updatedReview.ratings,
+      reviewId,
+    ]);
+    return updatedReviewData;
+  } catch (error) {
+    console.error("Error updating review:", error);
+    throw error;
+  }
+};
+
+const deleteStudentReview = async (reviewId) => {
+  try {
+    const query = `
+      DELETE FROM student_reviews
+      WHERE id = $1
+      RETURNING *`;
+    const deletedReviewData = await db.one(query, [reviewId]);
+    return deletedReviewData;
+  } catch (error) {
+    console.error("Error deleting review:", error);
+    throw error;
+  }
+};
+
+
 module.exports = {
   createStudentReview,
   showStudentReviewBasedOnTutor,
+  updateStudentReview,
+  deleteStudentReview
 };
