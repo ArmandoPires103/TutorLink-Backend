fea97bbbefbe015f8f63be642d53e419734c30e7
Carlitos <carlitosdutan@pursuit.org>
2024-03-29 17:49:05 -0400

    added post query and post route

--- controllers/requestsController.js
+++ controllers/requestsController.js
@@ -1,20 +1,35 @@
 // requestsController.js
-const express = require('express');
+const express = require("express");
 const requestsRouter = express.Router();
-const { getRequestsById } = require("../queries/requests");
+const {
+  getRequestsById,
+  createRequestByStudent,
+} = require("../queries/requests");
 
 // renamed fx for clarity - we don't need a get all because this is particular to logged in tutor
-requestsRouter.get('/:id', async (req, res) => {
+requestsRouter.get("/:id", async (req, res) => {
   const { id } = req.params;
 
   try {
     const requests = await getRequestsById(id);
     res.json(requests);
   } catch (error) {
-    console.error('Error fetching requests:', error);
-    res.status(500).json({ error: 'Internal Server Error' });
+    console.error("Error fetching requests:", error);
+    res.status(500).json({ error: "Internal Server Error" });
   }
 });
 
+requestsRouter.post("/:tutorId", async (req, res) => {
+  const { tutorId } = req.params;
+  const { student_id: userId } = req.body;
+
+  try {
+    const createdRequest = await createRequestByStudent(tutorId, userId);
+    res.status(201).json(createdRequest);
+  } catch (error) {
+    console.error("Error creating request:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
 
 module.exports = requestsRouter;
--- queries/requests.js
+++ queries/requests.js
@@ -1,19 +1,51 @@
+// // queries/requests.js
+// const db = require('../db/dbConfig');
+
+// const getRequestsById = async (tutorId) => {
+//     console.log("query", tutorId)
+//     try {
+//         const tutorRequests = await db.any(
+//             "SELECT student.username AS student_name, student.id AS student_id, student.email AS student_email, student.profile_pic AS student_profile_pic, requests.accepted FROM requests INNER JOIN users AS tutor ON requests.tutor_id = tutor.id INNER JOIN users AS student ON requests.user_id = student.id WHERE tutor.id = $1",
+//             [tutorId]
+//         );
+//         console.log(tutorRequests)
+//         return tutorRequests;
+//     } catch (error) {
+//         console.error('Error fetching requests:', error);
+//         throw error;
+//     }
+// };
+
+// module.exports = { getRequestsById };
 // queries/requests.js
-const db = require('../db/dbConfig');
+const db = require("../db/dbConfig");
 
 const getRequestsById = async (tutorId) => {
-    console.log("query", tutorId)
-    try {
-        const tutorRequests = await db.any(
-            "SELECT student.username AS student_name, student.id AS student_id, student.email AS student_email, student.profile_pic AS student_profile_pic, requests.accepted FROM requests INNER JOIN users AS tutor ON requests.tutor_id = tutor.id INNER JOIN users AS student ON requests.user_id = student.id WHERE tutor.id = $1",
-            [tutorId]
-        );
-        console.log(tutorRequests)
-        return tutorRequests;
-    } catch (error) {
-        console.error('Error fetching requests:', error);
-        throw error;
-    }
+  console.log("query", tutorId);
+  try {
+    const tutorRequests = await db.any(
+      "SELECT requests.id AS request_id, student.username AS student_name, student.id AS student_id, student.email AS student_email, student.profile_pic AS student_profile_pic, requests.accepted FROM requests INNER JOIN users AS tutor ON requests.tutor_id = tutor.id INNER JOIN users AS student ON requests.user_id = student.id WHERE tutor.id = $1",
+      [tutorId]
+    );
+    console.log(tutorRequests);
+    return tutorRequests;
+  } catch (error) {
+    console.error("Error fetching requests:", error);
+    throw error;
+  }
 };
 
-module.exports = { getRequestsById };
+const createRequestByStudent = async (tutorId, userId) => {
+  try {
+    const newRequest = await db.one(
+      "INSERT INTO requests (user_id, tutor_id, accepted) VALUES ($1, $2, true) RETURNING *",
+      [userId, tutorId]
+    );
+    return newRequest;
+  } catch (error) {
+    console.error("Error creating request:", error);
+    throw error;
+  }
+};
+
+module.exports = { getRequestsById, createRequestByStudent };
